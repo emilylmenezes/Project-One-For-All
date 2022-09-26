6b25143ab71b44f55ba77cb63bd195890b8e8784
Emily Menezes <estudosmenezes@gmail.com>
2022-09-26 00:43:20 -0300

    Requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+	SELECT
+            ( SElECT COUNT(*) FROM SpotifyClone.musica ) AS 'cancoes',
+            ( SElECT COUNT(*) FROM SpotifyClone.artistas ) AS 'artistas',
+            ( SElECT COUNT(*) FROM SpotifyClone.album ) AS 'albuns';
